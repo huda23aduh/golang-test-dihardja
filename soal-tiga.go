4496752f57cc439fd3395eb436105f8c8ecf59a3
huda23aduh <huda23aduh@gmail.com>
2022-09-02 15:15:00 +0700

    commit soal-tiga

---
+++ soal-tiga.go
@@ -0,0 +1,45 @@
+
+package main
+
+    import (
+        "fmt"
+    )
+
+    func main(){
+        var res = minOpr(8)
+        fmt.Print("result = ")
+        fmt.Print(res)
+    }
+    
+    func minOpr(n int) (int) {
+        totalIdx := n+1
+        dp := make([]int, totalIdx)
+
+        
+        dp[1] = 0
+        
+        for i := 2; i <= n; i++ {
+            
+            dp[i] = 999999
+            
+            if i % 2 == 0 {
+                var x = dp[i / 2]
+                if (x + 1 < dp[i]) {
+                    dp[i] = x + 1;
+                }
+            }
+            
+            if i % 3 == 0 {
+                var x = dp[i / 3]
+                if (x + 1 < dp[i]) {
+                    dp[i] = x + 1;
+                }
+            }
+            
+            var x = dp[i-1]
+            if x+1 < dp[i] {
+                dp[i] = x + 1;
+            }
+        }
+        return dp[n]
+    }
